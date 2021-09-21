1c1dd02227458d87e83da94d288f887bab3024f9
vini <vinnicius9511@gmail.com>
2021-09-21 20:06:15 -0300

    inserting docker files for mysql

---
+++ mysql/db/createtables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users` (
+	`name` VARCHAR(255) NOT NULL,
+	`taxid` VARCHAR(14),
+	`nickname` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) NOT NULL,
+	`phone` VARCHAR(20) NOT NULL,
+	`gender` CHAR(1) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
