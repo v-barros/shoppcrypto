0218e5f810ce6140e3cd2bd291d578c38a572266
vini <vinnicius9511@gmail.com>
2021-09-21 22:02:58 -0300

    adding spring boot project structure

---
+++ src/main/java/com/shoppcrypto/shoppcrypto/ShoppcryptoApplication.java
@@ -0,0 +1,13 @@
+package com.shoppcrypto.shoppcrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppcryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppcryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppcrypto/shoppcrypto/ShoppcryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppcrypto.shoppcrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppcryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
