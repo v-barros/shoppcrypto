03ebf69247b913493e4784f8f4d4ab89a1e3c6ce
vini <vinnicius9511@gmail.com>
2021-09-23 11:45:05 -0300

    adding user Controller

---
+++ src/main/java/com/shoppcrypto/shoppcrypto/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.shoppcrypto.shoppcrypto.controller;
+
+
+import com.shoppcrypto.shoppcrypto.dto.UserDto;
+import com.shoppcrypto.shoppcrypto.form.UserForm;
+import com.shoppcrypto.shoppcrypto.model.User;
+import com.shoppcrypto.shoppcrypto.persistence.service.UserRepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController()
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserRepositoryService userRepositoryService;
+
+    private static final String path = "/users/{id}";
+
+    @PostMapping
+    private ResponseEntity<UserDto> saveUser(@RequestBody UserForm userForm, UriComponentsBuilder uriBuilder){
+        User user = new User(userForm);
+        Optional<User>  optionalUser = userRepositoryService.save(user);
+
+        if(optionalUser.isPresent()){
+            URI uri = uriBuilder.path(path).buildAndExpand(optionalUser.get().getId()).toUri();
+
+            return ResponseEntity.created(uri).body(new UserDto(optionalUser.get()));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
