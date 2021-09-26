78b174c143c14f547fc412f56b6d4377fcbbc5c8
vini <vinnicius9511@gmail.com>
2021-09-26 16:48:55 -0300

    adding userDetails and userDetailsService for user authentication

---
+++ src/main/java/com/shoppcrypto/shoppcrypto/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.shoppcrypto.shoppcrypto.config.security;
+
+import com.shoppcrypto.shoppcrypto.model.User;
+import com.shoppcrypto.shoppcrypto.persistence.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByEmail(username);
+
+        if(optionalUser.isEmpty()){
+            throw new UsernameNotFoundException("User "+username+" not found");
+        }
+        return new UserDetailsImpl(optionalUser);
+    }
+}
