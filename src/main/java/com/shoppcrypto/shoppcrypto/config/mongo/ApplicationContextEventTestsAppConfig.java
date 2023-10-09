7387db0e1ab8522d81bf0cf0bad97c311ac690ea
Vinicius Barros <vinnicius9511@gmail.com>
2023-10-08 21:16:28 -0300

    changing mongodb properties

--- src/main/java/com/shoppcrypto/shoppcrypto/config/mongo/ApplicationContextEventTestsAppConfig.java
+++ src/main/java/com/shoppcrypto/shoppcrypto/config/mongo/ApplicationContextEventTestsAppConfig.java
@@ -3,6 +3,8 @@ package com.shoppcrypto.shoppcrypto.config.mongo;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import com.shoppcrypto.shoppcrypto.config.ConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
@@ -16,13 +18,20 @@ public class ApplicationContextEventTestsAppConfig extends AbstractMongoClientCo
         return "shoppcrypto_dev";
     }
 
+    @Autowired
+    private ConfigProperties configProperties;
     @Override
     protected void configureClientSettings(MongoClientSettings.Builder builder) {
 
         builder
-                .credential(MongoCredential.createCredential("root", "shoppcrypto_dev", "123465".toCharArray()))
+                .credential(MongoCredential.createCredential(
+                        configProperties.getConfigValue("spring.data.mongodb.username"),
+                        configProperties.getConfigValue("spring.data.mongodb.database"),
+                        configProperties.getConfigValue("spring.data.mongodb.password").toCharArray()))
                 .applyToClusterSettings(settings  -> {
-                    settings.hosts(singletonList(new ServerAddress("127.0.0.1", 27017)));
+                    settings.hosts(singletonList(new ServerAddress(
+                            configProperties.getConfigValue("spring.data.mongodb.host"),
+                            Integer.parseInt(configProperties.getConfigValue("spring.data.mongodb.port")))));
                 });
     }
 }
